e2d6b9e090a8874ed174895630e2aecb92c6cad3
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-06-02 22:41:16 -0300

    clean server code and install knex and sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json())
 
-// app.use(express.json())
-app.get('/users', (req, res)=>{
-  res.json([
-    'Jonathan',
-    'Helena',
-    'Delaine',
-  ])
-});
+app.use(routes)
+
 
 app.listen(3333)
